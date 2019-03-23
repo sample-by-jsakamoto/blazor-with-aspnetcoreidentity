f8348b33fc6e8bbfadeb3bbf307c5cd30f642ff9
jsakamoto <junichi.jp@gmail.com>
2019-03-23 16:44:38 -0700

    Port "Home/Index" from server side MVC to client side Blazor.

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -10,11 +10,6 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
