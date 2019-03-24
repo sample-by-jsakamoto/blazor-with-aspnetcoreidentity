4b27b07eda0419d11b34073cb7e9c691ca322efd
jsakamoto <junichi.jp@gmail.com>
2019-03-23 18:22:07 -0700

    Port "About", "Contact" pages to client side Blazor.

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -10,20 +10,6 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
