18e3279181bcf23925bdcecb6624e5ca251adceb
jsakamoto <junichi.jp@gmail.com>
2019-03-23 17:11:01 -0700

    Add "UserIdentity" web api.

---
+++ BlazorApplication1.Shared/UserIdentity.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApplication1.Shared
+{
+    public class UserIdentity
+    {
+        public bool IsSignedIn { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication2/Controllers/UserIdentityController.cs
@@ -0,0 +1,37 @@
+﻿using BlazorApplication1.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class UserIdentityController : Controller
+    {
+        private SignInManager<ApplicationUser> SignInManager { get; }
+
+        private UserManager<ApplicationUser> UserManager { get; }
+
+        public UserIdentityController(
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager
+        )
+        {
+            SignInManager = signInManager;
+            UserManager = userManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public UserIdentity Get()
+        {
+            return new UserIdentity
+            {
+                IsSignedIn = SignInManager.IsSignedIn(User),
+                Name = UserManager.GetUserName(User)
+            };
+        }
+    }
+}
